2f2c14f2041ecfee7b667617a26145ea07d1a067
Keaton Sentak <ksentak01@gmail.com>
2022-08-29 22:10:05 -0400

    add validation

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,32 @@ const App: React.FC = () => {
   const [qrValue, setQrValue] = useState('');
 
   const generateQrCode = (): void => {
+    setIsDisplayingQrCode(false);
     setIsLoading(true);
-    setTimeout(() => {
+    const isValidQr = validateQrValue();
+
+    if (isValidQr) {
+      setTimeout(() => {
+        setIsLoading(false);
+        setIsDisplayingQrCode(true);
+      }, 1500);
+    } else {
       setIsLoading(false);
-      setIsDisplayingQrCode(true);
-    }, 1500);
+      alert('Please enter a valid URL');
+    }
   };
 
   const handleQrChange = (e: any): void => {
     setIsDisplayingQrCode(false);
     setQrValue(e.target.value);
+  };
+
+  const validateQrValue = (): boolean => {
+    const urlRegex =
+      '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+    const url = new RegExp(urlRegex, 'i');
+
+    return qrValue.length < 2083 && url.test(qrValue);
   };
 
   return (
